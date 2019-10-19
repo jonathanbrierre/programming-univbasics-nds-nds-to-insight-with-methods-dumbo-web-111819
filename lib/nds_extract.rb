494e319088038fc41e6cb39af130ef229c06825e
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-19 00:42:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,8 +27,8 @@ end
 def gross_for_director(director_data)
   movie_index = 0 
   gross_total = 0 
-  while movie_index <nds[director_data][:movies].lenth do
-     gross_total += nds[director_data][:movies][movie_index][:worldwide_gross]
+  while movie_index <directors_database[director_data][:movies].lenth do
+     gross_total += directors_database[director_data][:movies][movie_index][:worldwide_gross]
       movie_index += 1 
   end
   gross_total

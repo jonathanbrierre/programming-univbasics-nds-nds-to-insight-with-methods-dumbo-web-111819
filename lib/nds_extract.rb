22a58050d72223645a5713935efde8169020abd0
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-19 00:48:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,5 @@
 require 'directors_database'
-
+require 'pry'
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
 #
@@ -33,5 +33,5 @@ def gross_for_director(director_data)
       
   end
   gross_total
-  
+  binding.pry
 end

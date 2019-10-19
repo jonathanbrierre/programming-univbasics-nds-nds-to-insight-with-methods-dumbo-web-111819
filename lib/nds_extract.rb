8ff466a8f26ee216575b9789cee0193210f3bf06
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-19 00:05:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,6 +7,18 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
+  director_index = 0 
+  
+  while director_index < directors_database.count do
+    movie_index = 0 
+    gross_total = 0 
+    while movie_index < directors_database[director_index][:movies].length do
+      gross_total += directors_database[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
+    end
+    results[directors_database[director_index][:name]] = gross_total
+    director_index += 1 
+  end
   nil
 end
 
